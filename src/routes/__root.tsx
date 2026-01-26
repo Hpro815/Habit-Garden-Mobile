b6653ce5ba94bbb5963ae0f9d890729a06326d1a
Hpro815 <waterobttle2468@gmail.com>
2026-01-26 20:21:49 +0000

    update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,8 +8,8 @@ export const Route = createRootRoute({
 
 function Root() {
 	return (
-		<div className="flex flex-col min-h-screen">
-			<ErrorBoundary tagName="main" className="flex-1">
+		<div className="flex flex-col min-h-dvh bg-background">
+			<ErrorBoundary tagName="main" className="flex-1 pb-[env(safe-area-inset-bottom,0px)]">
 				<Outlet />
 			</ErrorBoundary>
 			<TanStackRouterDevtools position="bottom-right" />
